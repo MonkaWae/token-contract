2f546e0a3f3bc06d8c05fea16c27f87a1d7ba8cc
Aryan Malik <aryanhunny98@gmail.com>
2024-05-13 23:17:43 +0530

    Implemented Max Supply

--- MonkaWae.sol
+++ MonkaWae.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -9,6 +8,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract MonkaWae is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
+    // changeable max supply for now
+    uint256 public maxSupply = 9999999999999999 * 10 ** decimals();
+
     constructor(address initialOwner)
         ERC20("MonkaWae", "MKW")
         Ownable(initialOwner)
@@ -30,6 +32,11 @@ contract MonkaWae is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
     }
 
     // The following functions are overrides required by Solidity.
+
+    function _mint(address account, uint256 amount) internal override(ERC20) {
+        require(totalSupply() + amount <= maxSupply, "Max supply exceeded");
+        super._mint(account, amount);
+    }
 
     function _update(address from, address to, uint256 value)
         internal
